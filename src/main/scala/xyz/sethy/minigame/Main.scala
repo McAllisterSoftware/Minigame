801ea29bac779715b6328a93dd4738bf37d922e7
= <wout.ceulemans@gmail.com>
2017-04-29 18:51:51 +0200

    Refactored Team class further

--- src/main/scala/xyz/sethy/minigame/Main.scala
+++ src/main/scala/xyz/sethy/minigame/Main.scala
@@ -1,7 +1,9 @@
 package xyz.sethy.minigame
 
+import org.bukkit.{Bukkit, ChatColor}
 import org.bukkit.plugin.java.JavaPlugin
 import xyz.sethy.minigame.listeners.PreJoinListener
+import xyz.sethy.minigame.team.Team
 
 /**
   * Created by Seth on 29/04/2017.
